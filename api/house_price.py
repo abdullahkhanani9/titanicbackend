e1ed8189f71c2fbb1b6820105bea496afa1d7dd5
devaSas1 <devadharson@hotmail.com>
2024-03-28 10:13:31 -0700

    rounding

--- api/house_price.py
+++ api/house_price.py
@@ -79,66 +79,13 @@ class HousePriceAPI(Resource):
             area, bedrooms, bathrooms, stories, mainroad, guestroom, basement, hotwaterheating, airconditioning, parking, prefarea = self.preprocess_inputs(area, bedrooms, bathrooms, stories, mainroad, guestroom, basement, hotwaterheating, airconditioning, parking, prefarea)
             # Predict house price
             predicted_price = self.predict_house_price(area, bedrooms, bathrooms, stories, mainroad, guestroom, basement, hotwaterheating, airconditioning, parking, prefarea)
-            return jsonify({'predicted_price': predicted_price/10})
+            predicted_price = predicted_price/10
+            predicted_price = round(predicted_price, 2)
+
+            return jsonify({'predicted_price': predicted_price})
         except Exception as e:
             return jsonify({'error': str(e)})
 
 api.add_resource(HousePriceAPI, '/predict')
 
 
-"""
-# Load the dataset
-data = pd.read_csv('house_prices.csv')
-
-# Preprocessing and feature selection/engineering
-# Remove the 'furnishingstatus' column
-X = data.drop(['price', 'furnishingstatus'], axis=1)
-y = data['price']
-
-# Train the model
-model = LinearRegression()
-model.fit(X, y)
-
-# Save the trained model
-with open('trained_model.pkl', 'wb') as file:
-    pickle.dump(model, file)
-
-# Define a function to make predictions
-def predict_house_price(area, bedrooms, bathrooms, stories, mainroad, guestroom, basement, hotwaterheating, airconditioning, parking, prefarea):
-    # Prepare input data
-    input_data = pd.DataFrame({
-        'area': [area],
-        'bedrooms': [bedrooms],
-        'bathrooms': [bathrooms],
-        'stories': [stories],
-        'mainroad': [mainroad],
-        'guestroom': [guestroom],
-        'basement': [basement],
-        'hotwaterheating': [hotwaterheating],
-        'airconditioning': [airconditioning],
-        'parking': [parking],
-        'prefarea': [prefarea]
-    })
-    
-    # Make prediction
-    with open('trained_model.pkl', 'rb') as file:
-        model = pickle.load(file)
-        predicted_price = model.predict(input_data)
-    
-    return predicted_price[0]
-
-area = float(input("Enter the area of the house (in sqft): "))
-bedrooms = int(input("Enter the number of bedrooms: "))
-bathrooms = int(input("Enter the number of bathrooms: "))
-stories = int(input("Enter the number of stories: "))
-mainroad = int(input("Is the house located on the main road? (1 for Yes, 0 for No): "))
-guestroom = int(input("Does the house have a guest room? (1 for Yes, 0 for No): "))
-basement = int(input("Does the house have a basement? (1 for Yes, 0 for No): "))
-hotwaterheating = int(input("Does the house have hot water heating? (1 for Yes, 0 for No): "))
-airconditioning = int(input("Does the house have air conditioning? (1 for Yes, 0 for No): "))
-parking = int(input("Enter the number of parking spaces: "))
-prefarea = int(input("Is the house in a preferred area? (1 for Yes, 0 for No): "))
-
-predicted_price = predict_house_price(area, bedrooms, bathrooms, stories, mainroad, guestroom, basement, hotwaterheating, airconditioning, parking, prefarea)
-print("Predicted price:", predicted_price/10)
-"""
